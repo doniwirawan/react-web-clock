c67f26139192656363b0b26fc623348052c718f0
“doniwirawan” <doniwirawan166@gmail.com>
2022-05-11 18:46:52 +0800

    adding padding

--- src/Components/Note.js
+++ src/Components/Note.js
@@ -17,7 +17,7 @@ const Note = () => {
     }, [])
 
     return (
-        <div>
+        <div className='sm:py-0 md:py-8'>
             <form onSubmit={handleSubmit} className='flex sm:w-full md:w-2/5 h-13 m-auto '>
                 <input type={'text'} className='border-b-2 focus:outline-none focus:outline-none  border-slate-700 mt-5 w-4/5 px-4 py-2 placeholder:text-left placeholder:font-light placeholder:text-lg text-lg font-light' placeholder='What is your focus for today?' onChange={handleChange} value={text}></input>
                 <button type='submit' className='bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ease-in-out hover:ease-in py-1 px-6  text-center m-auto block mt-9 ml-2 text-gray-100 hover:text-gray-200 rounded-sm hover:bg-sky-700  font-semibold '>Submit</button>
